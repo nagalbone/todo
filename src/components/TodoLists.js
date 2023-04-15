4b8b11d0e4efddbf142e261895e2c014385fcb16
Arvind <arvindnagalbone@gmail.com>
2023-04-15 20:47:27 +0530

    Add cross for delete button

--- src/components/TodoLists.js
+++ src/components/TodoLists.js
@@ -2,7 +2,7 @@ const TodoLists = (props) =>{
     return(
         <>
             <ul>
-                <li>{props.items} <button onClick={()=>props.fun(props.index)}>del</button></li>
+                <li>{props.items} <button onClick={()=>props.fun(props.index)}>X</button></li>
             </ul>
         </>
     )
